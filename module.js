da71d93f6d1ca7dd20af1e5a382310d9c6a98306
rank88928 <qq889281@gmail.com>
2024-01-28 23:14:01 +0800

    css

--- module.js
+++ module.js
@@ -1,20 +1,45 @@
-document.addEventListener('DOMContentLoaded', function () {
+// document.addEventListener('DOMContentLoaded', function () {
     
-    fetch("header.html")
-    // 使用 Fetch API 加載外部 HTML 檔案
+//     fetch("header.html")
+//     // 使用 Fetch API 加載外部 HTML 檔案
+//         .then(response => response.text())
+//         // 把該對象轉換為純文本
+//         // 注意 這裡會相關到Promise(指成功或未成功解析的暫存對象) 
+//         // Promise 物件代表一個即將完成、或失敗的非同步操作，以及它所產生的值。
+//         .then(data => {
+//             document.getElementById("header").innerHTML = data;
+//         });
+//         // 將取得的 HTML 內容插入到 id 為 "header" 的元素中
+
+//     fetch("footer.html")
+//         .then(response => response.text())
+//         .then(data => {
+//             // 將取得的 HTML 內容插入到 id 為 "footer" 的元素中
+//             document.getElementById("footer").innerHTML = data;
+//         });
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+        // 操作目標
+    fetch("share.html")
+        // 載入的檔案
         .then(response => response.text())
         // 把該對象轉換為純文本
         // 注意 這裡會相關到Promise(指成功或未成功解析的暫存對象) 
         // Promise 物件代表一個即將完成、或失敗的非同步操作，以及它所產生的值。
-        .then(data => {
-            document.getElementById("header").innerHTML = data;
-        });
-        // 將取得的 HTML 內容插入到 id 為 "header" 的元素中
 
-    fetch("footer.html")
-        .then(response => response.text())
         .then(data => {
-            // 將取得的 HTML 內容插入到 id 為 "footer" 的元素中
-            document.getElementById("footer").innerHTML = data;
+            // 創建一個虛擬的 HTML 文檔
+            const virtualDocument = new DOMParser().parseFromString(data, 'text/html');
+
+            // 使用 importNode 方法將 header 和 footer 的父元素引入到當前文檔
+            const Header = document.importNode(virtualDocument.getElementById("header"), true);
+            const Footer = document.importNode(virtualDocument.getElementById("footer"), true);
+
+            // 將引入的元素插入到目標元素中
+            const top = document.getElementById("header");
+            top.appendChild(Header);
+            const bot = document.getElementById("footer");
+            bot.appendChild(Footer);
         });
 });
