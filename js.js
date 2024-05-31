9dd598bcb3345abd8eecbb919b2af4c2074894eb
rank88928 <qq889281@gmail.com>
2024-05-31 21:59:38 +0800

    [revise]-login-box

--- js.js
+++ js.js
@@ -2,44 +2,44 @@ let nav_menu_but = document.querySelector(".menu-but");
 let nav_menu = document.querySelector(".nav");
 let menu_mask = document.querySelector(".menu-mask");
 
-if(nav_menu_but){
-    nav_menu_but.addEventListener("click", function(){
+if (nav_menu_but) {
+    nav_menu_but.addEventListener("click", function () {
         nav_menu.classList.toggle("open");
         menu_mask.classList.toggle("open");
     });
 }
 
 
-let login = document.querySelector(".login-mask");
-let loginOpen = document.querySelector(".login-open");
-let closelogin = document.querySelector(".close-login");
-
-if(login){
-    loginOpen.addEventListener("click", function(){
-        login.style.display ="block";
-    });
-    closelogin.addEventListener("click", function(){
-        login.style.display ="none";
-    });
-}
 
 
-let qboxes = document.querySelectorAll(".qa-Click-box");
-let aboxes = document.querySelectorAll(".qa-a");
-let arrows = document.querySelectorAll(".fa-angle-up");
+// 問答手風琴
+let accordion_item = document.querySelectorAll(".accordion-item");
+let question = document.querySelectorAll(".question");
+let question_i = document.querySelectorAll(".question i");
+let answer = document.querySelectorAll(".answer");
 
-if(qboxes){
-    qboxes.forEach(function(qbox, index) {
-        qbox.addEventListener('click', function (){
-            // 藉由添加類名修改CSS狀態
-            aboxes[index].classList.toggle('hidden');
-            arrows[index].classList.toggle('rotate-180');
-            // 問答框-點擊後下展示框
-            // 使用 forEach 遍歷 NodeList
-    
+if (accordion_item) {
+    accordion_item.forEach(function (question, i) {
+        question.addEventListener("click", function () {
+
+            let state = answer[i].classList.contains("h-auto");
+
+            for (let i = 0; i < answer.length; i++) {
+                answer[i].classList.remove("h-auto");
+                question_i[i].classList.remove("down-rotate");
+            };
+            // 每次只顯示一個 如果要個別打開收合 只需留下面toggle判斷狀態 不需要if
+
+            if (!state) {
+                answer[i].classList.toggle("h-auto");
+                question_i[i].classList.toggle("down-rotate");
+            }
+            // 一開始沒有顯示才新增標籤
         });
     });
-}
+};
+
+
 
 
 
