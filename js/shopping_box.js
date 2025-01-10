54d52687eaee3c6194027ae836aabb9ce41cc0f3
rank88928 <57557150+rank88928@users.noreply.github.com>
2025-01-11 01:07:00 +0800

    Update shopping_box.js

--- js/shopping_box.js
+++ js/shopping_box.js
@@ -21,7 +21,7 @@ shopping_item_box.addEventListener('click', function (e) {
 // 更新購物欄
 function update_cart_box() {
     let shopping_records = utils.get_local_records('shopping_records');
-
+    console.log(shopping_records)
     let shopping_box = document.querySelector('.shopping-item');
     shopping_box.innerHTML = '';
 
